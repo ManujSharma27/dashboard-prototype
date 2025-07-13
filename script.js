aa10155db2036f23b81d67212fa3f4290d42a6d3
ManujSharma27 <160638460+ManujSharma27@users.noreply.github.com>
2025-07-13 16:57:40 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const box = document.querySelector('.test-box');
-    if (box) {
-        box.textContent = 'JS Loaded!';
-        console.log('Script running at ' + new Date().toLocaleTimeString());
-    } else {
-        console.log('Box not found at ' + new Date().toLocaleTimeString());
-    }
-    const cssLink = document.querySelector('link[href="./styles.css"]');
-    if (cssLink && cssLink.sheet) {
-        console.log('CSS loaded at ' + new Date().toLocaleTimeString());
-    } else {
-        console.log('CSS not loaded at ' + new Date().toLocaleTimeString());
-    }
+    console.log('Script loaded at ' + new Date().toLocaleTimeString());
 });
